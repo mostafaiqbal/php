93eba080041a28afe11ae5ea9b81350e90955d07
M Iqbal <rashel.edu@gmail.com>
2018-08-03 18:23:59 +0100

    post

--- getpost.php
+++ getpost.php
@@ -1,14 +1,29 @@
 <?php
-//GET
 
+ if($_POST) {
+
+    $group = array('imran', 'iqbal', 'shakil', 'hossain', 'tayeb');
+    $isMember = false;
+
+    for ($i=0; $i < sizeof($group); $i++) { 
+
+        if($_POST['name'] == $group[$i]) {
+            $isMember = true;
+        } 
+    } 
+
+    if($isMember) {
+        echo 'Hello';
+    } else {
+        echo 'Entry needed';
+    }
+  }
 ?>
 
-<h1> Prime Number Checker </h1> 
-<form>
+<h1> Group Member Checker </h1> 
 
-
-
-    <input name="number" type = "text"> 
+<form method = 'post'>
+    <input name="name" type = "text"> 
     <input type="submit" value="Submit">
 
 </form>
