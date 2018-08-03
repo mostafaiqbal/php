ed3bf61a6c698beaea26fdb705a3c6147a510e58
M Iqbal <rashel.edu@gmail.com>
2018-08-03 19:17:35 +0100

    contact us

--- email.php
+++ email.php
@@ -1,6 +1,18 @@
 <?
 
+$emailTo = "i.rashel@outlook.com";
+$subject = "Hello from Other Side" ;
+$body = "Hi this is me testing php email";
+$headers = "From: iqbal@marblecars.co.uk" ;
 
-
+if (mail($emailTo, $subject, $body, $headers)) {
+    echo "Sent Successfully";
+} else {
+    echo "Failed";
+} 
 
 ?>
+
+
+
+<h1> Send Email </h1> 
