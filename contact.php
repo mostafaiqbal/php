a0dd1824797693ebe1e311edb749d858ec37291e
M Iqbal <rashel.edu@gmail.com>
2018-08-14 22:04:36 +0100

    'php'

--- contact.php
+++ contact.php
@@ -1,81 +1,155 @@
 <?
 
+    // Created by Mostafa Iqbal (github.com/mostafaiqbal)
+    //Present to You by MiQ iNNOV@TiON (github.com/miqinnovation)
+    //This codes are free to use
+    
+    $error = "";
+    $successMessage = "";
+    
+    if($_POST) {
+
+        if (!$_POST["email"]) {
+
+            $error.= "Email address is required <br/>";
+
+        }
+        if (!$_POST["subject"]) {
+
+            $error.= "Subject filed is required <br/>";
+
+        }
+        if (!$_POST["content"]) {
+
+            $error.= "Content filed is required";
+
+        }
+
+        if ($_POST["email"] && filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) === false ) {
+            $error.= "The email address is invalid <br/>"; 
+          }
+
+        if($error != "") {
+
+            $error = '<div class="alert alert-danger" role="alert"><strong> There were error(s) in your form:</strong> <br/>' . $error. '</div>';
+
+        } else {
+            $emailTo = "test@shopnookbd.com";
+            $subject = $_POST["subject"];
+            $content = $_POST["content"];
+            $headers = "From:" .$_POST["email"];
+
+            if(mail($emailTo, $subject, $content, $headers)) {
+
+                $successMessage = '<div class="alert alert-success role="alert"><strong> Your Message has been Sent Successfully </div>';
+            } else {
+                $error = '<div class="alert alert-danger" role="alert"><strong> Message Failed. Please try again </div>';
+            }
+        }
+
+    }
 
 
 ?>
 
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!doctype html>
+    <html lang="en">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>Contact Us</title>
-  </head>
-  <body>
-    <div class = "container" > 
-        
-        <h1>Contact Us </h1>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+            crossorigin="anonymous">
 
-        <div id="error" role="alert" > </div>
+        <title>Contact Us</title>
+    </head>
 
-        <form method="post">
-        <fieldset class="form-group">
-            <label for="email">Email address</label>
-            <input type="email" class="form-control" id="email" name="email" placeholder="Enter email">
-            <small class="text-muted">We'll never share your email with anyone else.</small>
-        </fieldset>
-        <fieldset class="form-group">
-            <label for="subject">Subject</label>
-            <input type="text" class="form-control" id="subject" name="subject" >
-        </fieldset>
-        <fieldset class="form-group">
-            <label for="exampleTextarea">What would you like to ask us?</label>
-            <textarea class="form-control" id="content" name="content" rows="3"></textarea>
-        </fieldset>
-        <button type="submit" id="submit" class="btn btn-primary">Submit</button>
-        </form>
-    </div> 
+    <body>
+        <div class="container">
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+            <h1>Contact Us </h1>
 
-    <script type="text/javascript"> 
-        $("form").submit(function(e){
-        e.preventDefault();
+            <div id="error">
+                <? echo $error. $successMessage; ?>
 
-        var error = "";
+            </div>
 
-        if ($("#email").val() == "") {
-            error += "<p> Email address is required. <p>"
-        }        
-          
-        if ($("#subject").val() == "") {
-            error += "<p> Subject field is required. <p>"
-        }
+            <form method="post">
+                <fieldset class="form-group">
+                    <label for="email">Email address</label>
+                    <input type="email" class="form-control" id="email" name="email" placeholder="Enter email">
+                    <small class="text-muted">We'll never share your email with anyone else.</small>
+                </fieldset>
+                <fieldset class="form-group">
+                    <label for="subject">Subject</label>
+                    <input type="text" class="form-control" id="subject" name="subject">
+                </fieldset>
+                <fieldset class="form-group">
+                    <label for="exampleTextarea">What would you like to ask us?</label>
+                    <textarea class="form-control" id="content" name="content" rows="3"></textarea>
+                </fieldset>
+                <button type="submit" id="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
 
-        if ($("#content").val() == "") {
-            error += "<p> Content field is required. <p>"
-        }
-        
-       if($("error") != "") {
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous">
+        </script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+            crossorigin="anonymous">
+        </script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+            crossorigin="anonymous">
+        </script>
 
-     $("#error").html('<div class="alert alert-danger" role="alert"> <strong>There are error(s) </strong> + + </div>');
-    
-     }
+        <script type="text/javascript">
+            $("form").submit(function(e) {
+                let error = "";
 
-    });
+                $("#error").html(error)
 
-    </script> 
+                if ($("#email").val() == "") {
+
+                    error += "Email address is required <br/>"
+
+                }
+
+                $("#error").html(error)
+
+                if ($("#subject").val() == "") {
+
+                    error += "The subject filed is required <br/>"
+
+                }
+
+                $("#error").html(error)
+
+                if ($("#content").val() == "") {
+
+                    error += "<p> Content is required"
+
+                }
+
+                if (error != "") {
+
+                    $("#error").html(
+                        '<div class="alert alert-danger" role="alert"><strong> There were error(s) in your form:</strong> <br/>' +
+                        error + '</div>');
+                    return false;
+                } else {
+                    return true;
+                }
+
+            });
+        </script>
 
 
 
-  </body>
-</html>
+    </body>
+
+    </html>
